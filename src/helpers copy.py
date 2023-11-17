344d3389977edc253c18055515316418df36558c
medimed66 <mehdi.elbouari@epfl.ch>
2023-11-17 23:26:29 +0100

    Section 3

--- src/helpers copy.py
+++ src/helpers copy.py
@@ -1,3 +1,8 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
 import spacy
 from spacy.tokens import Token
 
@@ -48,7 +53,7 @@ def replace_pronouns(doc, charSex, characters):
                         theyStack.append([token.text, token.nbor(3).text])
                         neutralStack.append([token.text, token.nbor(3).text])
                 elif any(charSex.index.str.contains(token.text)):
-                    sex=charSex.loc[charSex.index.str.contains(token.text)]["Actor gender"].values[0]
+                    sex=charSex.loc[charSex.index.str.contains(token.text)]["actor_gender"].values[0]
                     if sex=="F":
                         sheStack.append(token.text)
                         neutralStack.append(token.text)
@@ -82,26 +87,66 @@ def get_adjectives_for_character(character, doc):
                 adjectives.append(token.text)
     return adjectives
 
-#function that returns every subject, verb, and object set if present
-def get_subject_object(doc):
+#function that given a sentence returns the subject, verb, and object if present
+def get_subject_verb_object(sent):
+    actions=[]
+    for token in sent:
+        if token.pos_ == "VERB" :  # Root verb (action)
+            action = token.text
+            subject = [child.text for child in token.children if child.dep_ == "nsubj" or child.dep_ == "nsubjpass"]
+            object_ = [child.text for child in token.children if child.dep_ == "dobj"]
+            if subject:
+                actions.append((subject[0], action, object_[0] if object_ else None))
+                return subject[0], action, object_[0] if object_ else None
+
+def get_interaction(doc, nlp, characters):
     interactions=[]
-    for token in doc:
-        if token.pos_=="VERB":
-            subj=[child for child in token.children if child.dep_ == "nsubj" or child.dep_ == "nsubjpass"]
-            subj=subj[0] if subj else None
-            obj=[child for child in token.children if child.dep_ == "dobj" or child.dep_ == "pobj"]
-            obj=obj[0] if obj else None
-            if subj and obj:
-                if subj.text in characters and obj.text in characters:
-                    interactions.append([subj.text, token.lemma_, obj.text])
-                elif subj._.ref:
-                    for elem in subj._.ref:
-                        if obj._.ref:
-                            for elem2 in obj._.ref:
-                                interactions.append([elem, token.lemma_, elem2])
-                        else:
-                            interactions.append([elem, token.lemma_, obj.text])
-                elif obj._.ref:
-                    for elem in obj._.ref:
-                        interactions.append([subj.text, token.lemma_, elem])
+    for character in characters:
+        for sent in get_sentences_with_person(character, doc):
+            for character2 in characters:
+                if character2 != character and character2 in sent:
+                    doc2=nlp(sent)
+                    main_verb = [token.text for token in doc2 if (token.pos_ == "VERB") and (token.dep_ == "ROOT")]
+                    if len(main_verb) > 0:
+                        main_verb = main_verb[0]
+                        if [character, main_verb, character2] not in interactions and [character2, main_verb, character] not in interactions:
+                            interactions.append([character, main_verb, character2])
     return interactions
+
+def edge_center_coordinates(edge, curvature, pos):
+    start_x, start_y = pos[edge[0]]
+    end_x, end_y = pos[edge[1]]
+    # Calculate the control point coordinates for the curved edge
+    control_x = 0.5 * (start_x + end_x) + curvature * (end_y - start_y)
+    control_y = 0.5 * (start_y + end_y) - curvature * (end_x - start_x)
+    # Calculate the Bezier curve parameters
+    t = 0.5  # Midpoint of the curve
+    bx = (1 - t)**2 * start_x + 2 * (1 - t) * t * control_x + t**2 * end_x
+    by = (1 - t)**2 * start_y + 2 * (1 - t) * t * control_y + t**2 * end_y
+    return bx, by
+
+def graph_plot(data):
+    G=nx.MultiDiGraph()
+    plt.figure(figsize=(10,10))
+    for subj in data["Char1"].unique():
+        G.add_node(subj, color="lightblue")
+        i=0
+        for verb, obj in data[data["Char1"]==subj][["Verb", "Char2"]].itertuples(index=False):
+            G.add_edge(subj, obj, label=verb, curvature=i/30)
+            i+=2
+    pos = nx.circular_layout(G)
+    # Draw nodes separately
+    nx.draw_networkx_nodes(G, pos, node_size=2000, node_color='lightblue')
+    # Draw node labels if needed
+    nx.draw_networkx_labels(G, pos, font_size=10, font_color='black')
+    # Draw the graph with curved edges based on the 'curvature' attribute
+    for edge in G.edges(data=True):
+        edge_data = edge[2]
+        label = edge_data['label']
+        curvature = edge_data.get('curvature', 0.1)  # Default curvature if 'curvature' is not present
+        #draw edges
+        nx.draw_networkx_edges(G, pos, edgelist=[(edge[0], edge[1])], connectionstyle=f'arc3,rad={curvature}', edge_color='black', width=2, alpha=0.7, label=label)
+        center_coordinates = edge_center_coordinates(edge, curvature, pos)
+        plt.text(center_coordinates[0], center_coordinates[1], label, color='red', fontsize=8, ha='center', va='center', bbox=dict(facecolor='white', alpha=0.7, edgecolor='none'))
+    # Show the plot
+    plt.show()
